4cfc2d82aa1c7303b7e45e719e7ef75a1c30f549
Drágos Norbert <79311672+dragosnorbert439@users.noreply.github.com>
2022-05-31 17:21:58 +0300

    Chore: Updated AuthController
    + added register post method

--- src/main/java/com/msglearning/javabackend/controllers/AuthController.java
+++ src/main/java/com/msglearning/javabackend/controllers/AuthController.java
@@ -1,16 +1,13 @@
 package com.msglearning.javabackend.controllers;
 
 import com.msglearning.javabackend.entity.User;
-import com.msglearning.javabackend.services.PasswordService;
 import com.msglearning.javabackend.services.TokenService;
 import com.msglearning.javabackend.services.UserService;
+import com.msglearning.javabackend.to.UserTO;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.Optional;
@@ -20,6 +17,7 @@ import java.util.Optional;
 public class AuthController {
     public static final String AUTHORIZATION = "authorization";
     private static final String LOGIN_PATH = "/login";
+    public static final String REGISTER_PATH = "/register";
 
     @Autowired
     UserService userService;
@@ -30,9 +28,15 @@ public class AuthController {
     @PostMapping(LOGIN_PATH)
     public String login(@RequestHeader Map<String, String> headers) {
 
-        String decodedAuthHeader = new String(Base64.decodeBase64(
-                headers.get(AUTHORIZATION).substring(6)
-        ));
+        String decodedAuthHeader;
+        try {
+            decodedAuthHeader = new String(Base64.decodeBase64(
+                    headers.get(AUTHORIZATION).substring(6)
+            ));
+        }
+        catch (NullPointerException e) {
+            return "Forbidden";
+        }
         String email = decodedAuthHeader.substring(0, decodedAuthHeader.indexOf(":"));
         String password = decodedAuthHeader.substring(decodedAuthHeader.indexOf(":") + 1);
 
@@ -48,5 +52,16 @@ public class AuthController {
         }
 
         return "Forbidden";
+    }
+
+    @PostMapping(REGISTER_PATH)
+    public boolean register(@RequestBody UserTO userTO) {
+        try {
+            userService.save(userTO);
+        }
+        catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 }
